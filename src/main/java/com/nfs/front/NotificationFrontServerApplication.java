b38dcdd85de411d7de96878214e8e442aa3fb7f9
yeoseon.yun <yeoseon.yun@nhndooray.com>
2024-05-15 15:48:59 +0900

    project init

---
+++ src/main/java/com/nfs/front/NotificationFrontServerApplication.java
@@ -0,0 +1,13 @@
+package com.nfs.front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationFrontServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationFrontServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nfs/front/NotificationFrontServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nfs.front;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationFrontServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
